c2f4e23bc71ae2980d2535927f2a1d7c984b8155
giang19062001 <85018900+giang19062001@users.noreply.github.com>
2023-02-27 15:13:25 +0700

    update fe 27/2 3:13

--- client/src/component/user/footer.jsx
+++ client/src/component/user/footer.jsx
@@ -2,10 +2,10 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import { Box, Grid, Container } from "@mui/material";
 import Typography from "@mui/material/Typography";
-import PlaceIcon from '@mui/icons-material/Place';
+import PlaceIcon from "@mui/icons-material/Place";
 import PhoneIcon from "@mui/icons-material/Phone";
 import EmailIcon from "@mui/icons-material/Email";
-import FacebookIcon from '@mui/icons-material/Facebook';
+import FacebookIcon from "@mui/icons-material/Facebook";
 const Footer = () => {
   return (
     <Box sx={{ bottom: 0, width: "100%" }}>
@@ -13,59 +13,71 @@ const Footer = () => {
         <Container>
           <Grid container spacing={6}>
             <Grid item md={4}>
-                <img
-                  src={require("../../assets/logoKingH.jpg")}
-                  alt=""
-                  className="mx-auto w-40 rounded-full"
-                ></img>
-                <Typography className="text-neutral-900 font-bold my-6" align="center">KING HOUSE - Nâng tầm giá trị sống</Typography>
-                <Typography className="text-neutral-900 font-bold"  align="center" >
-                &copy; Website be develop by <a className="text-red-700" href="/"> ASTROTECH.VN </a>
-                </Typography>
+              <img
+                src={require("../../assets/logoKingH.jpg")}
+                alt=""
+                className="mx-auto w-40 rounded-full"
+              ></img>
+              <Typography
+                className="text-neutral-900 font-bold my-6"
+                align="center"
+              >
+                KING HOUSE - Nâng tầm giá trị sống
+              </Typography>
+              <Typography className="text-neutral-900 font-bold" align="center">
+                &copy; Website be develop by{" "}
+                <a className="text-red-700" href="/">
+                  {" "}
+                  ASTROTECH.VN{" "}
+                </a>
+              </Typography>
             </Grid>
             <Grid item md={4}>
-            
-                <Typography className="text-neutral-900 font-bold pb-3">
-                  HOTLINE
-                </Typography>
-                <Typography className="bg-slate-50 rounded-full text-neutral-900 font-bold p-2">
-                &emsp;   <PhoneIcon></PhoneIcon> &emsp;{process.env.REACT_APP_PHONE}
-                </Typography>
-                <Typography className="text-neutral-900 font-bold py-3">
-                  EMAIL
-                </Typography>
-                <Typography className="bg-slate-50 rounded-full text-neutral-900 font-bold p-2">
-                 &emsp;  <EmailIcon></EmailIcon> &emsp; {process.env.REACT_APP_EMAIL}
-                </Typography>
-                <Typography className="text-neutral-900 font-bold py-3">
-                  ĐỊA CHỈ
-                </Typography>
-                <Typography align="center" className="bg-slate-50 rounded-full text-neutral-900  font-bold p-2 ">
-               <PlaceIcon></PlaceIcon>
-                  &emsp;{process.env.REACT_APP_ADDRESS}
-                </Typography>
+              <Typography className="text-neutral-900 font-bold pb-3">
+                HOTLINE
+              </Typography>
+              <Typography className="bg-slate-50 rounded-full text-neutral-900 font-bold p-2">
+                &emsp; <PhoneIcon></PhoneIcon> &emsp;
+                {process.env.REACT_APP_AUTH_PHONE}
+              </Typography>
+              <Typography className="text-neutral-900 font-bold pb-3 pt-8">
+                EMAIL
+              </Typography>
+              <Typography className="bg-slate-50 rounded-full text-neutral-900 font-bold p-2">
+                &emsp; <EmailIcon></EmailIcon> &emsp;{" "}
+                {process.env.REACT_APP_EMAIL}
+              </Typography>
             </Grid>
             <Grid item md={4}>
-                
+            <Typography className="text-neutral-900 font-bold pb-3">
+                FACEBOOK
+              </Typography>
+            <Typography
+                align="center"
+                className="bg-slate-50 rounded-full text-sky-500 font-bold p-2"
+              >
+                <a href={process.env.REACT_APP_FB}>
+                  <FacebookIcon></FacebookIcon>
+                  &emsp;FACEBOOK
+                </a>
+              </Typography>
+              <Typography className="text-neutral-900 font-bold  pb-3 pt-8">
+                ĐỊA CHỈ
+              </Typography>
+              <Typography
+                align="center"
+                className="bg-slate-50 rounded-full text-neutral-900  font-bold p-2 "
+              >
+                <PlaceIcon></PlaceIcon>
+                &emsp;{process.env.REACT_APP_ADDRESS}
+              </Typography>
 
-
-                <Typography align="center" className="bg-slate-50 rounded-full text-sky-500 font-bold p-2 mt-10">
-               <FacebookIcon></FacebookIcon>
-                  &emsp;FACEBOOK 
-                </Typography>
-                <Typography align="center" className="bg-slate-50 rounded-full text-sky-500 font-bold p-2 mt-11 ">
-                <i class="fas fa-phone-square-alt"></i>
-                  &emsp;ZALO
-                </Typography>
-                
-            </Grid>
             
+            </Grid>
           </Grid>
-         
         </Container>
       </AppBar>
     </Box>
-
   );
 };
 export default Footer;
